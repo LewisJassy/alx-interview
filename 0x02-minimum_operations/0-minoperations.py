e5191570c8e49cf2862c1b3c70f6b16fb2dc9fbd
LewisJassy <jassylewis70@gmail.com>
2024-07-14 21:37:19 +0300

    chore: Refactor minOperations function for improved readability and performance

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,19 +3,46 @@
 '''
 
 
-def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result in exactly n 'H' characters in the file.
+    
+    The allowed operations are:
+    - Copy All: Copy all the characters present in the file.
+    - Paste: Paste the copied characters.
+    
+    Args:
+    n (int): The target number of 'H' characters.
+    
+    Returns:
+    int: The minimum number of operations required to achieve exactly n 'H' characters.
+         If n is impossible to achieve, returns 0.
+    
+    Example:
+    >>> minOperations(9)
+    6
+    
+    Explanation:
+    H => Copy All => Paste => HH => Paste => HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+    Number of operations: 6
+    """
+    
+    # If n is less than or equal to 1, it's impossible to achieve more than 1 'H'
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    # Loop to factorize n and count the operations
+    while n > 1:
+        # Check if the current divisor is a factor of n
+        while n % divisor == 0:
+            # If it is, increment the operations count by the divisor
+            operations += divisor
+            # Divide n by the divisor to reduce it
+            n //= divisor
+        # Move to the next possible divisor
+        divisor += 1
+    
+    return operations
