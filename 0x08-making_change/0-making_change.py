49f1acabce6a2f3c094a2bd1fc0e8a20c568610b
LewisJassy <jassylewis70@gmail.com>
2024-08-22 20:59:52 +0300

    Refactor makeChange function to sort coins in descending order

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """"python module"""
 
+
 def makeChange(coins, total):
     """function to determine the fewest coins to make total"""
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     num_coins = 0
     for coin in coins:
