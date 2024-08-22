f48b8c746d353fcc6a78abfa9d77a3309a77d0ca
LewisJassy <jassylewis70@gmail.com>
2024-08-22 20:55:01 +0300

    Add makeChange function for determining the fewest coins to make a total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""python module"""
+
+def makeChange(coins, total):
+    """function to determine the fewest coins to make total"""
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        num_coins += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return num_coins
